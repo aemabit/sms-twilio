dd4c1d8061f2cd5db30e08530606c11c493ae3cc
Andres Mejias <aemabit@gmail.com>
2019-12-23 12:57:32 -0500

    create view for msg

---
+++ client/src/smsView.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import axios from 'axios'
+
+
+export default function SendSms(){
+    //const [number, setNumber] = React.useState(null)
+    //const [msg, setMsg] = React.useState("")
+    //const [loading, setLoading] = React.useState(false)
+//
+    return(
+        <div>
+            Hello from Msg!!!
+        </div>
+    )
+}
